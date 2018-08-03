88ffa50dd66fa70dcd3a8dbae9d83469e61b1319
RicardoCapa <33547541+RicardoCapa@users.noreply.github.com>
2018-08-03 01:20:30 -0500

    Add files via upload

---
+++ PanicButtom/app.js
@@ -0,0 +1,9 @@
+var exp = require('express');
+var app=exp();
+var path = require('path');
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
